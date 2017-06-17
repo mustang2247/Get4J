7b6b1d83af237bb8ed658e70c98eb999057b8c2f
bytegriffin <bytegriffin@126.com>
2017-06-17 11:07:44 +0800

    ｆｉｘ

--- get4j-sample/src/main/java/com/bytegriffin/get4j/sample/QQNewsPageParser.java
+++ get4j-sample/src/main/java/com/bytegriffin/get4j/sample/QQNewsPageParser.java
@@ -1,6 +1,7 @@
 package com.bytegriffin.get4j.sample;
 
 import com.bytegriffin.get4j.Spider;
+import com.bytegriffin.get4j.annotation.Field;
 import com.bytegriffin.get4j.core.Page;
 import com.bytegriffin.get4j.parse.PageParser;
 
@@ -9,14 +10,17 @@ import com.bytegriffin.get4j.parse.PageParser;
  */
 public class QQNewsPageParser implements PageParser {
 
+	@Field("span.a_catalog>a[href]")
+	private String catalog;
+
     @Override
     public void parse(Page page) {
-       System.err.println(page.getTitle() + "   " + page.getUrl() );
+       System.err.println(page.getField("catalog") + "   " + page.getUrl() );
     }
 
     public static void main(String[] args) throws Exception {
         Spider.list_detail().fetchUrl("http://news.qq.com/c/2013ywList_{1}.htm").detailSelector("a.linkto[href]")
-                .parser(AnjukePageParser.class)
+                .parser(QQNewsPageParser.class).field(QQNewsPageParser.class)
                 .thread(1).start();
     }
 
