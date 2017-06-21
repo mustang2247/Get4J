1f518a140e2449bf085f9c1013274d414c283a9d
bytegriffin <bytegriffin@126.com>
2017-06-21 12:54:49 +0800

    更新jdk8时间api

--- get4j-core/src/main/java/com/bytegriffin/get4j/util/DateUtil.java
+++ get4j-core/src/main/java/com/bytegriffin/get4j/util/DateUtil.java
@@ -1,8 +1,11 @@
 package com.bytegriffin.get4j.util;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,10 +18,13 @@ public final class DateUtil {
     public static final String yyyyMMddHHmmss = "yyyy-MM-dd HH:mm:ss";
 
     public static Date strToDate(String str) {
-    	SimpleDateFormat format = new SimpleDateFormat(yyyyMMddHHmmss);
+    	DateTimeFormatter format = DateTimeFormatter.ofPattern(yyyyMMddHHmmss);
         try {
-            return format.parse(str);
-        } catch (ParseException e) {
+        	LocalDateTime ldt = LocalDateTime.parse(str, format);
+        	//通过instant做中转换，转换成Date
+            Instant instant =  ldt.atZone(ZoneId.systemDefault()).toInstant();
+            return Date.from(instant);
+        } catch (DateTimeParseException e) {
             logger.error("时间格式出错，正确格式为[yyyy-MM-dd HH:mm:ss]：", e);
             System.exit(1);
         }
@@ -29,14 +35,8 @@ public final class DateUtil {
      * 当前时间
      */
     public static String getCurrentDate() {
-        Date date = Calendar.getInstance().getTime();
-        SimpleDateFormat format = new SimpleDateFormat(yyyyMMddHHmmss);
-        return format.format(date);
-    }
-
-    public static String dateToStr(Date date) {
-        SimpleDateFormat format = new SimpleDateFormat(yyyyMMddHHmmss);
-        return format.format(date);
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern(yyyyMMddHHmmss);
+    	return LocalDateTime.now().format(formatter);
     }
 
     /**
@@ -46,43 +46,11 @@ public final class DateUtil {
      * @return String
      */
     public static String getCostDate(String startTime) {
-        String endTime = getCurrentDate();
-        SimpleDateFormat dfs = new SimpleDateFormat(yyyyMMddHHmmss);
-        long between = 0;
-        try {
-            Date begin = dfs.parse(startTime);
-            Date end = dfs.parse(endTime);
-            between = (end.getTime() - begin.getTime());// 得到两者的毫秒数
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        long day = between / (24 * 60 * 60 * 1000);
-        long hour = (between / (60 * 60 * 1000) - day * 24);
-        long min = ((between / (60 * 1000)) - day * 24 * 60 - hour * 60);
-        long s = (between / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
-        long ms = (between - day * 24 * 60 * 60 * 1000 - hour * 60 * 60 * 1000 - min * 60 * 1000 - s * 1000);
-        StringBuilder sb = new StringBuilder();
-        if (day > 0) {
-            sb.append(day);
-            sb.append("天");
-        }
-        if (hour > 0) {
-            sb.append(hour);
-            sb.append("小时");
-        }
-        if (min > 0) {
-            sb.append(min);
-            sb.append("分钟");
-        }
-        if (s > 0) {
-            sb.append(s);
-            sb.append("秒");
-        }
-        if (ms > 0) {
-            sb.append(ms);
-            sb.append("毫秒");
-        }
-        return sb.toString();
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern(yyyyMMddHHmmss);
+        LocalDateTime startDateTime = LocalDateTime.parse(startTime, formatter);
+        LocalDateTime endDateTime = LocalDateTime.now();
+        long seconds = ChronoUnit.SECONDS.between(startDateTime, endDateTime);
+        return seconds+"秒";
     }
 
 }
